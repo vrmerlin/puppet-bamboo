d69a7e9305bc76baa5ecb063c78b3e7ccfdde5a0
Josh Beard <josh@signalboxes.net>
2015-10-07 12:17:27 -0600

    More syntax cleanup
    * Handle empty proxy settings better for the Augeas change set

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -34,13 +34,6 @@ class bamboo::configure (
     content => "bamboo.home=${homedir}",
   }
 
-  if !empty($proxy) {
-    $_proxy = suffix(prefix(join_keys_to_values($proxy, " '"), 'set Server/Service/Connector/#attribute/'), "'")
-  }
-  else {
-    $_proxy = undef
-  }
-
   $changes = [
     "set Server/Service[#attribute/name='Catalina']/Engine/Host/Context/#attribute/path '${context_path}'",
     "set Server/Service/Connector/#attribute/maxThreads '${max_threads}'",
@@ -50,7 +43,14 @@ class bamboo::configure (
     "set Server/Service/Connector/#attribute/acceptCount '${accept_count}'",
   ]
 
-  $_changes = delete_undef_values(concat($changes, $_proxy))
+  if !empty($proxy) {
+    $_proxy   = suffix(prefix(join_keys_to_values($proxy, " '"), 'set Server/Service/Connector/#attribute/'), "'")
+    $_changes = concat($changes, $_proxy)
+  }
+  else {
+    $_proxy   = undef
+    $_changes = $changes
+  }
 
   augeas { "${app_dir}/conf/server.xml":
     lens    => 'Xml.lns',
